f1a5b1967953928abff4ccf74b6d9fe99fb28f91
Qiang Kou <qkou@umail.iu.edu>
2015-08-09 21:13:08 -0400

    small fix

--- script_by_rubenvb/scripts/rtmp.sh
+++ script_by_rubenvb/scripts/rtmp.sh
@@ -9,7 +9,7 @@ then
 else
   echo "--> Configuring $BUILD_DIR"
   make CROSS_COMPILE=$HOST- SYS=mingw CFLAGS="-O2 -I$BUILD_DIR/include" LDFLAGS="-L$BUILD_DIR/lib" all
-  cp librtmp.a $BUILD_DIR/lib
+  make prefix=$BUILD_DIR CROSS_COMPILE=$HOST- SYS=mingw CFLAGS="-O2 -I$BUILD_DIR/include" LDFLAGS="-L$BUILD_DIR/lib" install
   echo "--> installed"
 fi
 touch $LOG_DIR/rtmp_configure.marker
